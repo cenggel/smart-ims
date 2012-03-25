f3d8cd121c35bc249deb06f2607779f9f2524a96
lifuan.china@gmail.com <lifuan.china@gmail.com@8381702d-d415-f466-2c57-7c1d7b9b454c>
2012-03-25 11:54:24 +0000

    --by lifuan 全局变量声明，今后客户端的全局变量，全部定义在YCJ_DATA.js中。 自己封装的类，可以封装在YCJ_FUN.js中

--- html/scripts/YCJ_FUN.js
+++ html/scripts/YCJ_FUN.js
@@ -1,15 +1,3 @@
 
 //TAB组件封装
-//--全局变量声明
-JCJ_datas=
-{
-	tabs:
-    {
-       attendance  :  {key_word:"考勤",src="./attendance.html"},
-       blog        :  {key_word:"微博",src="./logger.html"},
-       post        :  {key_word:"公告",src="./post.html"},
-       memorandum  :  {key_word:"备忘",src="./memorandum.html"},
-       documents   :  {key_word:"文档",src="./documents.html"},
-       loger       :  {key_word:"日志",src="./logger.html"}
-    }
-}
+
